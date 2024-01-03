3278a736013f281afb5f6d780fa2d07c059dd54d
Ognian <ognianov26@gmail.com>
2024-01-03 19:02:51 +0200

    Add KFC marker and update the map

--- trailsapp/main/views.py
+++ trailsapp/main/views.py
@@ -5,12 +5,23 @@ from django.shortcuts import render
 
 # Create your views here.
 def my_first_view(request: HttpRequest):
-    m = folium.Map(location=(42.6654086, 23.2597745), zoom_start=20, tiles="cartodb positron")
+    m = folium.Map(
+                location=(42.6654086, 23.2597745),
+                # zoom_start=20,
+                tiles="OpenStreetMap"
+                   )
     folium.Marker(
         location=[42.6654086, 23.2597745],
         tooltip="Zenobia Baby",
         popup="Zenobia Baby",
         icon=folium.Icon(icon="heart"),
+    ).add_to(m)
+    folium.Marker(
+        location=[42.6531609, 23.365499],
+        tooltip="KFC",
+        popup="MMMMMMMMMMMMMMMMMMMMMMMM KFC",
+        icon=folium.Icon(icon="drumstick-bite", color="red", icon_color="white", prefix="fa", angle=45),
+
     ).add_to(m)
 
     context = {
