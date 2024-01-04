8bed3aa0138d5b776511a9af68bd380fe4768924
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-01-04 17:18:36 +0200

    update home view

--- trailsapp/main/views.py
+++ trailsapp/main/views.py
@@ -4,12 +4,10 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def my_first_view(request: HttpRequest):
-    m = folium.Map(
-                location=(42.6654086, 23.2597745),
-                # zoom_start=20,
-                tiles="OpenStreetMap"
-                   )
+
+
+def home_view(request: HttpRequest):
+    m = folium.Map(location=(42.6654086, 23.2597745), zoom_start=20, tiles="cartodb positron")
     folium.Marker(
         location=[42.6654086, 23.2597745],
         tooltip="Zenobia Baby",
