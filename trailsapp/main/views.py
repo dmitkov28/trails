870ff15998b720624fbcb4b0c4f92a4358d03f25
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-02-03 11:20:49 +0200

    add custom js button

--- trailsapp/main/views.py
+++ trailsapp/main/views.py
@@ -3,10 +3,13 @@ from django.http import HttpRequest
 from django.shortcuts import render
 from folium.plugins.fullscreen import Fullscreen
 from folium.plugins.locate_control import LocateControl
+from trailsapp.main.utils.js_button import JsButton
 
 
 def home_view(request: HttpRequest):
-    m = folium.Map(location=(42.6654086, 23.2597745), zoom_start=10, tiles="OpenStreetMap")
+    m = folium.Map(
+        location=(42.6654086, 23.2597745), zoom_start=10, tiles="OpenStreetMap"
+    )
 
     LocateControl(auto_start=True, keepCurrentZoomLevel=True).add_to(m)
 
@@ -24,6 +27,16 @@ def home_view(request: HttpRequest):
         icon=folium.Icon(icon="heart"),
     ).add_to(m)
 
+    # Add custom JS Button to Map
+    JsButton(
+        title='<span>Click</span>',
+        function="""
+    function(btn, map) {
+       alert('It works!');
+    }
+    """,
+    ).add_to(m)
+
     # folium.Marker(
     #     location=[42.656817785193056, 23.34933134976066],
     #     tooltip="Дани локация",
@@ -32,15 +45,14 @@ def home_view(request: HttpRequest):
     # ).add_to(m)
 
     folium.Marker(
-
         location=[42.65463735755751, 23.370681409879392],
         tooltip="KFC",
         popup="MMMMMMMMMMMMMMMMMMMMMMMM KFC",
-        icon=folium.Icon(icon="drumstick-bite", color="red", icon_color="white", prefix="fa"),
+        icon=folium.Icon(
+            icon="drumstick-bite", color="red", icon_color="white", prefix="fa"
+        ),
     ).add_to(m)
 
-    context = {
-        "map": m._repr_html_()
-    }
+    context = {"map": m._repr_html_()}
 
-    return render(request=request, template_name='index.html', context=context)
+    return render(request=request, template_name="index.html", context=context)
